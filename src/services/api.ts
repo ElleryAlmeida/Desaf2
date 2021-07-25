00ab4242e74c0632ef78bbba8828e1da4df2f684
ElleryAlmeida <elleryalmeida@gmail.com>
2021-07-25 19:12:22 -0300

    alterado site api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/produtos'
+    baseURL: 'https://github.com/ElleryAlmeida/Desaf2/blob/main/server.json'
 })
 
 export default api;
